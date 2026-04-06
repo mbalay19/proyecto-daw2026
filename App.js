25dacc99648be51e38baeae965ede8a389878b40
Mbalay19 <matias.balay.g@gmail.com>
2026-04-06 12:25:48 +0200

    Commit 1 - App.js

---
+++ App.js
@@ -0,0 +1,38 @@
+import express, { json } from 'express'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import moodRouter from './backend/routes/moods.js'
+
+dotenv.config()
+
+const app = express()
+
+// Meter el cookieparser y middleware antes de las rutas
+
+app.use(cookieParser())
+app.use(cors({
+  origin: 'http://localhost:4444',
+  credentials: true
+}))
+
+app.use(express.static('frontend'))
+app.use(express.urlencoded({ extended: true }))
+app.use(json())
+app.disable('x-powered-by') // Deshabilitar el header "X-Powered-By" para mejorar la seguridad
+
+// Configuración de seguridad adicional para XSS, Xframe, etc.
+app.use((req, res, next) => {
+  res.setHeader('X-Frame-Options', 'DENY') // Evitar que la página sea cargada en un iframe
+  res.setHeader('X-XSS-Protection', '1; mode=block') // Habilitar la protección contra XSS en navegadores compatibles
+  res.setHeader('X-Content-Type-Options', 'nosniff') // Evitar que el navegador interprete archivos como un tipo diferente al declarado
+  next()
+})
+
+app.use('/', moodRouter)
+
+const PUERTO = process.env.PORT ?? 2345
+
+app.listen(PUERTO, () => {
+  console.log(`Servidor escuchando en el puerto ${PUERTO}`)
+})
